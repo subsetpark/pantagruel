944886798f3b3694459b322e4b117b6d374cea37
Zach Smith <zd@zdsmith.com>
2018-08-14 10:27:26 -0400

    Working parser for (all?) function heads

--- logexian/lib/logexian.ex
+++ logexian/lib/logexian.ex
@@ -1,41 +1,89 @@
 defmodule ParserHelpers do
   import NimbleParsec
-  defparsec :comma_join, string(",") |> optional |> ignore |> times(min: 1)
+
+  def ignore_spaces(c \\ empty()) do
+    c
+    |> ignore(
+      choice([string(" "), string("\n")])
+      |> repeat()
+    )
+  end
+
+  def join(c, joiner) do
+    repeat(
+      c
+      |> ignore(joiner)
+      |> ignore_spaces()
+    )
+    |> times(
+      ignore_spaces()
+      |> concat(c)
+      |> ignore_spaces(),
+      min: 1
+    )
+  end
+
+  def comma_join(c), do: join(c, string(","))
+
+  def optional_bunch(c \\ empty()) do
+    ignore(optional(string("(")))
+    |> ignore_spaces()
+    |> concat(c)
+    |> comma_join
+    |> ignore_spaces()
+    |> ignore(optional(string(")")))
+  end
 end
+
 defmodule Logexian do
-    @moduledoc """
+  @moduledoc """
   Documentation for Logexian.
   """
   import NimbleParsec
   import ParserHelpers
 
+  where = string(";;\n")
 
-  where = ignore(optional(string(";;\n")))
+  identifier =
+    ignore(repeat(string(" ")))
+    |> ascii_string([?a..?z], min: 1)
+    |> ignore_spaces()
 
-  identifier = ignore(repeat(string(" ")))
-  |> ascii_string([?a..?z], min: 1)
-  |> ignore(repeat(string(" ")))
+  domain =
+    ignore(repeat(string(" ")))
+    |> ascii_string([?A..?Z, ?a..?z], min: 1)
+    |> ignore_spaces()
 
-  domain = ignore(repeat(string(" ")))
-  |> ascii_string([?A..?Z, ?a..?z], min: 1)
-  |> ignore(repeat(string(" ")))
+  function_vars = tag(optional_bunch(identifier), :decl_args)
+  function_doms = tag(optional_bunch(domain), :decl_doms)
 
-  function_vars = identifier
-  |> comma_join
-  |> wrap()
+  function_args =
+    function_vars
+    |> ignore(string(":"))
+    |> concat(function_doms)
 
-  function_doms = domain
-  domain
-  |> comma_join
-  |> wrap()
+  decl_yields =
+    ignore_spaces()
+    |> choice(for s <- ["::", "=>"], do: string(s))
+    |> unwrap_and_tag(:yield_type)
+    |> ignore_spaces()
+    |> concat(unwrap_and_tag(domain, :yield_domain))
 
-  function_args = function_vars
-  |> ignore(string(":"))
-  |> concat(function_doms)
+  decl =
+    identifier
+    |> unwrap_and_tag(:decl_ident)
+    |> ignore(string("<"))
+    |> concat(optional(function_args))
+    |> ignore(string(">"))
+    |> concat(optional(decl_yields))
+    |> ignore_spaces()
+    |> tag(:decl)
 
-  decl = ignore(string("<"))
-  |> concat(function_args)
-  |> ignore(string(">"))
-
-  defparsec :program, where |> concat(decl), debug: true
+  defparsec(
+    :program,
+    empty()
+    |> ignore_spaces()
+    |> concat(join(decl, where))
+    |> ignore_spaces()
+  )
 end
--- logexian/test/logexian_test.exs
+++ logexian/test/logexian_test.exs
@@ -2,9 +2,61 @@ defmodule LogexianTest do
   use ExUnit.Case
   doctest Logexian
 
-  test "greets the world" do
-    {:ok, [vars, doms], "", _, _, _} = Logexian.program(";;\n<a, b:B, C>")
-    assert vars == ["a", "b"]
-    assert doms == ["B", "C"]
+  defp do_decl_asserts(basic_decl) do
+    {:ok, [decl: [ident, vars, doms, yield_type, yield_domain]], "", _, _, _} =
+      Logexian.program(basic_decl)
+
+    assert ident == {:decl_ident, "f"}
+    assert vars == {:decl_args, ["a", "b"]}
+    assert doms == {:decl_doms, ["B", "C"]}
+    assert yield_type == {:yield_type, "::"}
+    assert yield_domain == {:yield_domain, "D"}
+  end
+
+  describe "declaration parsing" do
+    test "empty heading parsing" do
+      text = """
+      f <> => D 
+      """
+
+      {:ok,
+       [
+         decl: [
+           decl_ident: "f",
+           yield_type: "=>",
+           yield_domain: "D"
+         ]
+       ], "", %{}, _, _} = Logexian.program(text)
+    end
+
+    test "basic heading parsing" do
+      text = """
+      f<a, b:B, C> :: D 
+      """
+
+      do_decl_asserts(text)
+    end
+
+    test "optional parens heading parsing" do
+      text = """
+      f<(a, b):(B, C)>::D
+      """
+
+      do_decl_asserts(text)
+    end
+  end
+
+  describe "program structure" do
+    test "two declarations" do
+      text = "f<x: Y>\n;;\ny<> => Y"
+      {:ok, [decl: decl, decl: decl2], "", %{}, _, _} = Logexian.program(text)
+      assert [decl_ident: "f", decl_args: ["x"], decl_doms: ["Y"]] == decl
+
+      assert [
+               decl_ident: "y",
+               yield_type: "=>",
+               yield_domain: "Y"
+             ] == decl2
+    end
   end
 end
