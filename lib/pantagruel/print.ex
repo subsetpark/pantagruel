1ee4e4d00eb4c3204952108550e6e7146f7406ba
Zach Smith <zd@zdsmith.com>
2018-09-08 19:04:18 -0400

    Clean up print some more

--- lib/pantagruel/print.ex
+++ lib/pantagruel/print.ex
@@ -6,39 +6,51 @@ defmodule Pantagruel.Print do
   Generate a string representation of an evaluated program.
   """
   def print_program(parsed, scopes) do
+    bar = &String.duplicate(&1, 10)
+
     f = fn {section, scope} ->
       [print_scope(scope), print_section(section)]
-      |> Enum.join("\n#{bar("―")}\n")
+      |> Enum.join("\n#{bar.("―")}\n")
     end
 
     Enum.zip(parsed, scopes)
     |> Enum.map(f)
-    |> Enum.join("\n#{bar("═")}\n")
+    |> Enum.join("\n#{bar.("═")}\n")
   end
 
+  # Print the contents of the environment after program evaluation.
   defp print_scope(scope) do
     for(
       {_k, v} <- scope,
-      do: print_scope_element(v)
+      do:
+        case v do
+          %Lambda{} = l -> print_lambda(l)
+          e -> String.Chars.to_string(e)
+        end
     )
     |> Enum.join("\n")
   end
 
-  defp print_scope_element(%Lambda{} = l), do: print_lambda(l)
-  defp print_scope_element(e), do: String.Chars.to_string(e)
-
-  defp print_section(nil), do: ""
-
+  # Re-print the parsed program.
   defp print_section({:section, section}) do
+    print_line = fn {:expr, expression} ->
+      right_str = print_subexp(expression[:right])
+
+      case expression[:left] do
+        nil -> right_str
+        left -> "#{print_subexp(left)} ← #{right_str}"
+      end
+    end
+
     print_head = fn decls ->
       decls
-      |> Enum.map(&print_declaration/1)
+      |> Enum.map(&print_lambda/1)
       |> Enum.join("\n")
     end
 
     print_body = fn exprs ->
       exprs
-      |> Enum.map(&print_subbody/1)
+      |> Enum.map(print_line)
       |> Enum.join("\n")
     end
 
@@ -46,25 +58,47 @@ defmodule Pantagruel.Print do
     |> Enum.join("\n")
   end
 
-  defp print_declaration({:decl, declaration}) do
-    declaration
-    |> (fn d -> Lambda.from_declaration(d) end).()
-    |> print_lambda(declaration[:lambda_args])
+  def print_lambda(lambda, decl_args \\ nil)
+
+  def print_lambda(%Lambda{} = l, decl_args) do
+    name_str =
+      case l.name do
+        nil -> ""
+        name -> "#{name} : "
+      end
+
+    args_str =
+      case decl_args do
+        nil -> ""
+        args -> subexp_join(args) <> ":"
+      end
+
+    dom_str = subexp_join(l.domain)
+
+    yields_str =
+      case l.type do
+        nil -> ""
+        :function -> " :: "
+        :constructor -> " ⇒ "
+      end
+
+    name_str <> "|#{args_str}#{dom_str}|" <> yields_str <> print_subexp(l.codomain)
   end
 
-  defp print_subbody({:expr, expression}) do
-    right_str = print_subexpression(expression[:right])
+  def print_lambda({:decl, declaration}, _),
+    do: print_lambda(declaration, declaration[:lambda_args])
 
-    case expression[:left] do
-      nil -> right_str
-      left -> "#{print_subexpression(left)} ← #{right_str}"
-    end
+  def print_lambda(l, decl_args) do
+    Lambda.from_declaration(l) |> print_lambda(decl_args)
   end
 
-  defp print_subexpression(symbol) when is_binary(symbol) or is_number(symbol) or is_atom(symbol),
+  # Print an individual expression component.
+  defp print_subexp(nil), do: ""
+
+  defp print_subexp(symbol) when is_binary(symbol) or is_number(symbol) or is_atom(symbol),
     do: Env.lookup_binding_name(symbol)
 
-  defp print_subexpression({container, subexprs})
+  defp print_subexp({container, subexps})
        when container in [:bunch, :list, :string, :set] do
     {l, r} =
       case container do
@@ -74,77 +108,47 @@ defmodule Pantagruel.Print do
         :set -> {"{", "}"}
       end
 
-    inner_str = subexp_join(subexprs)
+    inner_str = subexp_join(subexps)
 
     "#{l}#{inner_str}#{r}"
   end
 
-  defp print_subexpression({:quantifier, [quantifier, binding, expr]}) do
+  defp print_subexp({:quantifier, [quantifier, binding, expr]}) do
     [
-      print_subexpression(quantifier),
+      print_subexp(quantifier),
       subexp_join(binding),
       "⸳",
-      print_subexpression(expr)
+      print_subexp(expr)
     ]
     |> Enum.join(" ")
   end
 
-  defp print_subexpression({:comprehension, [{container, [binding, expr]}]}) do
+  defp print_subexp({:comprehension, [{container, [binding, expr]}]}) do
     binding_str = subexp_join(binding)
-    expr_str = print_subexpression(expr)
+    expr_str = print_subexp(expr)
     inner_str = "#{binding_str} ⸳ #{expr_str}"
-    print_subexpression({container, [inner_str]})
+    print_subexp({container, [inner_str]})
   end
 
-  defp print_subexpression({:lambda, l}), do: print_declaration({:decl, l})
+  defp print_subexp({:lambda, l}), do: print_lambda(l)
 
-  defp print_subexpression({:literal, literal}) do
+  defp print_subexp({:literal, literal}) do
     cond do
       String.contains?(literal, " ") -> "`#{literal}`"
       true -> "`" <> literal
     end
   end
 
-  defp print_subexpression(subexpr) do
-    subexpr
-    |> Enum.map(&print_subexpression/1)
+  defp print_subexp(subexp) do
+    subexp
+    |> Enum.map(&print_subexp/1)
     |> Enum.join(" ")
     |> String.Chars.to_string()
   end
 
-  defp yields_string(nil, _), do: ""
-  defp yields_string(_, nil), do: ""
-  defp yields_string(:function, _), do: " :: "
-  defp yields_string(:constructor, _), do: " ⇒ "
-
-  defp codomain_string(nil), do: ""
-  defp codomain_string(codomain), do: print_subexpression(codomain)
-
-  def print_lambda(lambda, decl_args \\ nil) do
-    name_str =
-      case lambda.name do
-        nil -> ""
-        name -> "#{name} : "
-      end
-
-    args_str =
-      case decl_args do
-        nil -> ""
-        args -> "#{subexp_join(args)}:"
-      end
-
-    dom_str = subexp_join(lambda.domain)
-
-    name_str <>
-      "|#{args_str}#{dom_str}|" <>
-      yields_string(lambda.type, lambda.codomain) <> codomain_string(lambda.codomain)
-  end
-
-  defp bar(char), do: String.duplicate(char, 10)
-
   defp subexp_join(exprs) do
     exprs
-    |> Enum.map(&print_subexpression/1)
+    |> Enum.map(&print_subexp/1)
     |> Enum.join(", ")
   end
 end
