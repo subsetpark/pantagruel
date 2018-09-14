6524bda2c3cb413630aa00a697b1fb57bcf1b8e6
Zach Smith <zd@zdsmith.com>
2018-09-14 15:05:09 -0400

    Print out parsed version in case of failure too

--- lib/pantagruel.ex
+++ lib/pantagruel.ex
@@ -6,16 +6,21 @@ defmodule Pantagruel do
   end
 
   def main(filename) do
+    {:ok, parsed, "", %{}, _, _} = read!(filename)
+
     try do
-      {:ok, parsed, "", %{}, _, _} = read!(filename)
       scope = Pantagruel.Eval.eval(parsed)
 
       Pantagruel.Print.print_program(parsed, scope)
       |> IO.puts()
     rescue
       e in Pantagruel.Env.UnboundVariablesError ->
+        IO.puts("Eval error.\n")
         IO.puts("Unbound variables:")
         Enum.each(e.unbound, &IO.puts("- #{Pantagruel.Print.print_subexp(&1, e.scopes)}"))
+
+        Pantagruel.Print.print_program(parsed, for(_ <- parsed, do: []))
+        |> IO.puts()
     end
   end
 end
