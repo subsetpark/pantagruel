efdac22d2cfae348db368bd01ce864b263e15c8e
Zach Smith <zd@zdsmith.com>
2018-09-16 00:56:53 -0400

    Factor out CLI code

--- lib/pantagruel.ex
+++ lib/pantagruel.ex
@@ -1,4 +1,10 @@
 defmodule Pantagruel do
+  import IO, only: [puts: 1]
+  import Pantagruel.Format, only: [format_exp: 2, format_program: 2]
+
+  alias Pantagruel.{Scan, Parse, Eval}
+  alias Pantagruel.Env.{UnboundVariablesError, SymbolExtractionError}
+
   @moduledoc """
   An interpreter for the Pantagruel language.
 
@@ -10,40 +16,41 @@ defmodule Pantagruel do
   Generate an AST representation of the given Pantagruel file.
   """
   def read!(filename) do
-    File.read!(filename)
-    |> Pantagruel.Scan.scan()
-    |> Pantagruel.Parse.program()
+    filename
+    |> File.read!()
+    |> Scan.scan()
+    |> Parse.program()
   end
 
+  @doc """
+  Given a filename, evaluate it as a Pantagruel program, print out the
+  evaluated scope, and then pretty-print the program.
+  """
   def main(filename) do
     {:ok, parsed, "", %{}, _, _} = read!(filename)
 
     try do
-      scope = Pantagruel.Eval.eval(parsed)
-
-      Pantagruel.Format.format_program(parsed, scope)
-      |> IO.puts()
+      scope = Eval.eval(parsed)
+      format_program(parsed, scope) |> puts
     rescue
-      e in Pantagruel.Env.UnboundVariablesError ->
-        IO.puts("Eval error.\n")
-        IO.puts("Unbound variables:")
-        Enum.each(e.unbound, &IO.puts("- #{Pantagruel.Format.format_exp(&1, e.scopes)}"))
-
-        Pantagruel.Format.format_program(parsed, for(_ <- parsed, do: %{}))
-        |> IO.puts()
-
-      e in Pantagruel.Env.SymbolExtractionError ->
-        expr =
-          {:quantifier, quant_operator: "…", quant_bindings: e.bindings, quant_expression: e.expr}
-
-        IO.puts("Syntax error.\n")
-
-        IO.puts(
-          "Expected binding form. Found: \"#{Pantagruel.Format.format_exp(expr, [])}\""
-        )
-
-        Pantagruel.Format.format_program(parsed, for(_ <- parsed, do: %{}))
-        |> IO.puts()
+      e in UnboundVariablesError -> handle_unbound_variables(e, parsed)
+      e in SymbolExtractionError -> handle_bad_bindings(e, parsed)
     end
   end
+
+  defp handle_unbound_variables(e, parsed) do
+    puts("Eval error.\n\nUnbound variables:")
+    Enum.each(e.unbound, &puts("- #{format_exp(&1, e.scopes)}"))
+    puts_parsed(parsed)
+  end
+
+  defp handle_bad_bindings(e, parsed) do
+    expr =
+      {:quantifier, quant_operator: "…", quant_bindings: e.bindings, quant_expression: e.expr}
+
+    puts("Syntax error.\n\nExpected binding form. Found: \"#{format_exp(expr, [])}\"")
+    puts_parsed(parsed)
+  end
+
+  defp puts_parsed(parsed), do: parsed |> format_program(for(_ <- parsed, do: %{})) |> puts()
 end
