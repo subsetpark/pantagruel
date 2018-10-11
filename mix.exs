0f82379ccaf91eaf69fe84d002bfbed4dbef32a9
Zach Smith <zd@zdsmith.com>
2018-10-10 22:41:52 -0400

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pantagruel.MixProject do
   def project do
     [
       app: :pantagruel,
-      version: "0.0.4",
+      version: "0.0.5",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
