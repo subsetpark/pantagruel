76611a810e452fdd58723cd4e5158b9a1efe0610
Zach Smith <zd@zdsmith.com>
2019-01-11 00:28:37 -0500

    format

--- lib/bool_alg/bool_alg.ex
+++ lib/bool_alg/bool_alg.ex
@@ -3,32 +3,79 @@ import TypeClass
 defmodule TermMacros do
   defmacro deftrue(operation, result) do
     quote do
-      def reduce(%__MODULE__{op: unquote(operation), x: true, y: var!(q)}), do: unquote(result)
-      def reduce(%__MODULE__{op: unquote(operation), x: var!(q), y: true}), do: unquote(result)
+      def reduce(%__MODULE__{
+            op: unquote(operation),
+            x: true,
+            y: var!(q)
+          }) do
+        unquote(result)
+      end
+
+      def reduce(%__MODULE__{
+            op: unquote(operation),
+            x: var!(q),
+            y: true
+          }) do
+        unquote(result)
+      end
     end
   end
 
   defmacro deftrue(operation, result, result2) do
     quote do
-      def reduce(%__MODULE__{op: unquote(operation), x: true, y: var!(q)}), do: unquote(result)
-      def reduce(%__MODULE__{op: unquote(operation), x: var!(q), y: true}), do: unquote(result2)
+      def reduce(%__MODULE__{
+            op: unquote(operation),
+            x: true,
+            y: var!(q)
+          }) do
+        unquote(result)
+      end
+
+      def reduce(%__MODULE__{
+            op: unquote(operation),
+            x: var!(q),
+            y: true
+          }) do
+        unquote(result2)
+      end
     end
   end
 
   defmacro deffalse(operation, result) do
     quote do
-      def reduce(%__MODULE__{op: unquote(operation), x: false, y: var!(q)}), do: unquote(result)
-      def reduce(%__MODULE__{op: unquote(operation), x: var!(q), y: false}), do: unquote(result)
+      def reduce(%__MODULE__{
+            op: unquote(operation),
+            x: false,
+            y: var!(q)
+          }) do
+        unquote(result)
+      end
+
+      def reduce(%__MODULE__{
+            op: unquote(operation),
+            x: var!(q),
+            y: false
+          }) do
+        unquote(result)
+      end
     end
   end
 
   defmacro deffalse(operation, result, result2) do
     quote do
-      def reduce(%__MODULE__{op: unquote(operation), x: false, y: var!(q)}) do
+      def reduce(%__MODULE__{
+            op: unquote(operation),
+            x: false,
+            y: var!(q)
+          }) do
         unquote(result)
       end
 
-      def reduce(%__MODULE__{op: unquote(operation), x: var!(q), y: false}) do
+      def reduce(%__MODULE__{
+            op: unquote(operation),
+            x: var!(q),
+            y: false
+          }) do
         unquote(result2)
       end
     end
